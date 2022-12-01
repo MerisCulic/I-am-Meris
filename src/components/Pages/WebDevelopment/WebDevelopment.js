6e82c29fd23aaac84dac40543445a807097d9501
Meris Čulić <meris.culic@gmail.com>
2022-12-01 23:44:05 +0100

    Update web dev projects page

--- src/components/Pages/WebDevelopment/WebDevelopment.js
+++ src/components/Pages/WebDevelopment/WebDevelopment.js
@@ -6,6 +6,8 @@ import SmartBrain from "./Logos/SmartBrain.png";
 import GuessTheNumber from "./Logos/GuessTheNumber.png";
 import Robofriends from "./Logos/Robofriends.png";
 import BookBits from "./Logos/BookBits.png";
+import Graditelj from "./Logos/Graditelj.png";
+import PowerApps from "./Logos/PowerApps.png";
 
 
 const WebDevelopment = () => {
@@ -17,11 +19,13 @@ const WebDevelopment = () => {
                 <div className="bg-washed-green pa2 br4 fl w-100">
                     <div className="applistitem">
                         <img src={IamMeris} alt="" width="auto" className="applogo ma2 br3 shadow-3"/>
-                        <div className="ma2">
+                        <div className="appdesc ma2">
                             <p>
                                 Wanted a website of my own for quite a while now, so here it is. 
+                            </p>
+                            <p>
                                 It was built with React JS and is equipped with a resposive design. 
-                                Try it out on differently sized screens. 
+                                Try it out on differently sized screens.
                             </p>
                             <h4 className="viewCode pointer shadow-3 grow" 
                                 onClick={()=> window.open("https://github.com/MerisCulic/I-am-Meris", "_blank")}>
@@ -31,16 +35,34 @@ const WebDevelopment = () => {
                     </div>
                     <hr></hr>
                     <div className="applistitem">
+                        <img src={Graditelj} alt="" height="auto" className="applogo ma2 pointer br3 shadow-3 grow"
+                            onClick={()=> window.open("https://graditelj.net/", "_blank")}
+                        />
+                        <div className="appdesc ma2">
+                            <p>
+                                Graditelj's website - a construction company located in Jesenice, Slovenia. 
+                            </p>
+                            <p>
+                                Made from scratch with HTML, SASS & JS with implementations of Parallax.js and FullPage.js.
+                            </p>
+                            <h4 className="viewCode pointer shadow-3 grow" 
+                                onClick={()=> window.open("https://github.com/MerisCulic/Graditelj", "_blank")}>
+                                View Code
+                            </h4>
+                        </div>
+                    </div>
+                    <hr></hr>
+                    <div className="applistitem">
                         <img src={BookBits} alt="" width="auto" className="applogo ma2 pointer br3 shadow-3 grow"
                             onClick={()=> window.open("https://book-bits.herokuapp.com/", "_blank")}
                         />
-                        <div className="ma2">
+                        <div className="appdesc ma2">
                             <p>
                                 Have you stumbled upon a great idea, fact or quote while reading? Has that little bit of your book
                                 made you think, or maybe question things? Share it with others on Book Bits.
                             </p>
                             <p>
-                                Created with Python, flask and Bootstrap. Connected to AWS S3 for storing files.
+                                Created with Python, flask and Bootstrap.
                             </p>
                             <h4 className="viewCode pointer shadow-3 grow" 
                                 onClick={()=> window.open("https://github.com/MerisCulic/BookBits", "_blank")}>
@@ -50,10 +72,28 @@ const WebDevelopment = () => {
                     </div>
                     <hr></hr>
                     <div className="applistitem">
+                        <img src={PowerApps} alt="" height="auto" className="applogo ma2 pointer br3 shadow-3 grow"
+                            onClick={()=> window.open("https://merisculic.github.io/", "_blank")}
+                        />
+                        <div className="appdesc ma2">
+                            <p>
+                                A presentational web page for Microsoft's PowerApps.
+                            </p>
+                            <p>
+                                Created with HTML, SASS and a big spoon of animations.
+                            </p>
+                            <h4 className="viewCode pointer shadow-3 grow" 
+                                onClick={()=> window.open("https://github.com/MerisCulic/Graditelj", "_blank")}>
+                                View Code
+                            </h4>
+                        </div>
+                    </div>
+                    <hr></hr>
+                    <div className="applistitem">
                         <img src={SmartBrain} alt="" width="auto" className="applogo ma2 pointer br3 shadow-3 grow"
                             onClick={()=> window.open("https://smart-brain--face-det-app.herokuapp.com/", "_blank")}
                         />
-                        <div className="ma2">
+                        <div className="appdesc ma2">
                             <p>
                                 A face recognition app that uses the Clarifai API (application programming interface) to detect a face within an image.
                                 Find a photo of a face online and copy its URL into the input and try it out. 
@@ -72,7 +112,7 @@ const WebDevelopment = () => {
                         <img src={Robofriends} alt="" width="auto" className="applogo ma2 pointer br3 shadow-3 grow"
                             onClick={()=> window.open("https://robofriends-ztm.herokuapp.com/", "_blank")}
                         />
-                        <div className="ma2">
+                        <div className="appdesc ma2">
                             <p>A fast filtering app created with React (ZTM academy training).</p>
                             <h4 className="viewCode pointer shadow-3 grow" 
                                 onClick={()=> window.open("https://github.com/MerisCulic/Robofriends", "_blank")}>
@@ -85,7 +125,7 @@ const WebDevelopment = () => {
                         <img src={NinjaTech} alt="" width="auto" className="applogo ma2 pointer br3 shadow-3 grow"
                             onClick={()=> window.open("https://ninja-tech-forum.herokuapp.com/", "_blank")}
                         />
-                        <div className="ma2">
+                        <div className="appdesc ma2">
                             <p>A basic forum built with Python (Smart Ninja Web Development 2 training).</p>
                             <h4 className="viewCode pointer shadow-3 grow" 
                                 onClick={()=> window.open("https://github.com/MerisCulic/NinjaTechForum", "_blank")}>
@@ -98,7 +138,7 @@ const WebDevelopment = () => {
                         <img src={GuessTheNumber} alt="" width="auto" className="applogo ma2 pointer br3 shadow-3 grow"
                             onClick={()=> window.open("https://guess-the-secret-number-game.herokuapp.com/", "_blank")}
                         />
-                        <div className="ma2">
+                        <div className="appdesc ma2">
                             <p>See how many times it takes you to guess the correct number (Python, Smart Ninja training).</p>
                             <h4 className="viewCode pointer shadow-3 grow" 
                                 onClick={()=> window.open("https://github.com/MerisCulic/Guess_the_number", "_blank")}>
